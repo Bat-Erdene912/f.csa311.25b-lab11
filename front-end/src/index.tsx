a2e1cdc9a650537f80c9c1b262de99974d077b24
Bat-Erdene912 <erdenee0912@gmail.com>
2025-04-14 23:59:06 +0800

    Update index.tsx

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './App.css';
 import App from './App';
 
-/**
- * This is the entrance of your React app. It renders your application
- * at the HTML element with id="root". Most often, you do not want to
- * change this file.
- */
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-/**
- * Render your application, App is the component we define in App.tsx.
- */
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
