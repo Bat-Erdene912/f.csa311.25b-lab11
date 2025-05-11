5a4db98ff496474e126dcdac47e87166cd37ec8e
Bat-Erdene912 <erdenee0912@gmail.com>
2025-05-11 21:37:01 +0800

    Update index.tsx

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './App.css';
-import App from './App';
+import ReactDOM from 'react-dom/client'; // Using the modern `createRoot` API
+import App from './App'; // Import the main App component
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+// Find the HTML element with id "root"
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+// Render <App /> inside the <React.StrictMode> wrapper
 root.render(
   <React.StrictMode>
     <App />
